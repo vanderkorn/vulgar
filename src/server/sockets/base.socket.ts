4b1464709a4d504c78dd6c9aab4c0920ddc8aaec
datatypevoid <david.r.niciforovic@gmail.com>
2016-06-14 12:27:18 -0400

    refactor(socket): server socket functionality

---
+++ src/server/sockets/base.socket.ts
@@ -0,0 +1,33 @@
+// ```
+// base.ts
+// (c) 2016 David Newman
+// david.r.niciforovic@gmail.com
+// base.ts may be freely distributed under the MIT license
+// ```
+
+// *base.ts*
+
+// This file contains the most basic functionality for server Socket.io
+// functionality.
+export default (io) => {
+  io.sockets.on('connect', (socket: any) => {
+    let handle: any = 'unknown';
+    console.log(`{socket} [client:connect] ${ handle } client connected`);
+    socket.on('disconnect', () => {
+      console.log(`{socket} [client:disconnect] ${ handle } disconnected`);
+      io.emit('chat:message', `{socket} ${ handle } has left the channel`);
+    });
+    socket.on('chat:message', (message: any) =>  {
+      // Just relay all messages to everybody
+      io.emit('chat:message', `${ handle } : ${ message }`);
+      // Logging
+      console.log(`{socket} [chat:message] ${ handle } : ${ message }`);
+    });
+    socket.on('chat:register', (input: any) => {
+      handle = input;
+      console.log(`{socket} [client:register] ${ handle } has registered with the server`);
+      io.emit('chat:message', `{socket} ${ handle } has joined the channel`);
+    });
+  });
+
+};
