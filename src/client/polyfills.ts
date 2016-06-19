9dcf387930bf64f71ec0652989c93b1ac5cf6f83
datatypevoid <david.r.niciforovic@gmail.com>
2016-06-19 14:44:13 -0400

    refactor(polyfills): use import for zonejs and ts-helpers

--- src/client/polyfills.ts
+++ src/client/polyfills.ts
@@ -9,10 +9,10 @@
 // Prefer CoreJS over the polyfills above
 import 'core-js/es6';
 import 'core-js/es7/reflect';
-require('zone.js/dist/zone');
+import 'zone.js/dist/zone';
 
 // Typescript "emit helpers" polyfill
-require('ts-helpers');
+import 'ts-helpers';
 
 if ('production' === ENV) {
   // Production
