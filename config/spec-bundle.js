112f3096585128fecdd4ac54d45d26a748ded9a5
datatypevoid <david.r.niciforovic@gmail.com>
2016-06-19 15:10:43 -0400

    chore(spec-bundle): use direct polyfills

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -9,13 +9,9 @@
  * all here! Crazy huh. So we need to do some setup
 */
 Error.stackTraceLimit = Infinity;
-//require('phantomjs-polyfill');
-//require('es6-promise');
-//require('es6-shim');
-//require('es7-reflect-metadata');
 
-// Prefer `CoreJS` over the polyfills commented out above
-require('core-js');
+require('core-js/es6');
+require('core-js/es7/reflect');
 
 // Typescript "emit helpers" polyfill
 require('ts-helpers');
