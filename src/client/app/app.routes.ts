9a4eae1e6f6ad178831c589148f4260171dc5b35
datatypevoid <david.r.niciforovic@gmail.com>
2016-06-21 01:37:54 -0400

    chore(app.routes): protect recipes route with AuthGuard

--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -6,20 +6,20 @@ import { Recipes } from './recipes/recipes.component';
 import { RegisterComponent } from './register/register.component';
 import { Todo } from './todo/todo.component';
 
+import { AuthGuard } from './shared/auth.guard';
 import { CanDeactivateGuard } from './shared/interfaces/can-deactivate.interface';
 
 export const routes: RouterConfig = [
   { path: '',         component: Home },
   { path: 'home',     component: Home },
   { path: 'login',    component: LoginComponent },
-  { path: 'recipes',  component: Recipes },
+  { path: 'recipes',  component: Recipes, canActivate: [AuthGuard] },
   { path: 'register', component: RegisterComponent, canDeactivate: [CanDeactivateGuard] },
   { path: 'todo',     component: Todo },
   // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
   // make sure you match the component type string to the require in asyncRoutes
   { path: 'about', component: 'About' }
 ];
-
 
 export const asyncRoutes = {
   'About': require('es6-promise-loader!./about')
