57ba3d77d30b0ba5a84851b241f41de88a8a9f1f
datatypevoid <david.r.niciforovic@gmail.com>
2016-06-22 00:25:13 -0400

    refactor(app.routes): load aux components async

--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { RouterConfig } from '@angular/router';
 
 import { Home } from './home';
-import { LoginComponent } from './login/login.component';
 import { NoContent } from './no-content';
-import { RegisterComponent } from './register/register.component';
-import { Todo } from './todo/todo.component';
 
 import { AuthGuard } from './shared/auth.guard';
 import { CanDeactivateGuard } from './shared/interfaces/can-deactivate.interface';
@@ -12,13 +9,13 @@ import { CanDeactivateGuard } from './shared/interfaces/can-deactivate.interface
 export const routes: RouterConfig = [
   { path: '',         component: Home },
   { path: 'home',     component: Home },
-  { path: 'login',    component: LoginComponent },
-  { path: 'recipes',  component: 'Recipes', canActivate: [AuthGuard] },
-  { path: 'register', component: RegisterComponent, canDeactivate: [CanDeactivateGuard] },
-  { path: 'todo',     component: Todo },
   // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
   // make sure you match the component type string to the require in asyncRoutes
   { path: 'about',    component: 'About' },
+  { path: 'login',    component: 'LoginComponent' },
+  { path: 'recipes',  component: 'Recipes', canActivate: [AuthGuard] },
+  { path: 'register', component: 'RegisterComponent', canDeactivate: [CanDeactivateGuard] },
+  { path: 'todo',     component: 'Todo' },
 
   // Catch all other requests and redirect to app `404` view
   { path: '**',       component: NoContent },
@@ -28,12 +25,19 @@ export const routes: RouterConfig = [
 // the component correctly
 export const asyncRoutes: AsyncRoutes = {
   'About': require('es6-promise-loader!./about'),
-  'Recipes': require('es6-promise-loader!./recipes/recipes.component')
+  'LoginComponent': require('es6-promise-loader!./login/login.component'),
+  'Recipes': require('es6-promise-loader!./recipes/recipes.component'),
+  'RegisterComponent': require('es6-promise-loader!./register/register.component'),
+  'Todo': require('es6-promise-loader!./todo/todo.component'),
 };
 
 // An array of callbacks to be invoked after bootstrap to prefetch async routes
 export const prefetchRouteCallbacks: Array<Es6PromiseLoader | Function> = [
-  asyncRoutes['About', 'Recipes'] // es6-promise-loader returns a function
+  asyncRoutes[ 'About',
+               'LoginComponent',
+               'Recipes',
+               'RegisterComponent',
+               'Todo' ] // es6-promise-loader returns a function
 ];
 
 // Es6PromiseLoader and AsyncRoutes interfaces are defined in custom-typings
