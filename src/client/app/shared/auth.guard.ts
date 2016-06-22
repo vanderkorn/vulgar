dd74646f66541201b8848e9c27633e5ae67a54dd
datatypevoid <david.r.niciforovic@gmail.com>
2016-06-21 20:56:04 -0400

    refactor(auth.guard): use stored user auth from app state

--- src/client/app/shared/auth.guard.ts
+++ src/client/app/shared/auth.guard.ts
@@ -4,14 +4,15 @@ import { CanActivate,
          ActivatedRouteSnapshot,
          RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { AppState } from '../app.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private appState: AppState, private router: Router) {}
 
-  canActivate(next:  ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (this.authService.isAuthenticated === true) { return true; }
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.appState.get('isAuthenticated')) { return true; }
     /// DEBUG
     /// TODO: Remove this DEBUG statement
     console.log('User authentication failed! Redirecting user to login.');
