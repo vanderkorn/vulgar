ca4b125a39b07887f8893b8ccfb6faec838c64c7
datatypevoid <david.r.niciforovic@gmail.com>
2016-09-27 21:13:48 -0700

    feat: detail component

---
+++ src/client/app/+detail/detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'detail',
+  template: `<h1>Hello from Detail</h1>`
+})
+
+export class Detail {
+  
+}
---
+++ src/client/app/+detail/routes.ts
@@ -0,0 +1,11 @@
+import { Detail } from ',/detail.component';
+import { Index } from './index.component';
+
+// Async copmonents must be named routes for WebpackAsyncRoute
+export const routes = {
+  path: 'detail',
+  component: Index,
+  children: [
+    { path: '', component: Detail }
+  ]
+};
