3dcd8af9638d6665d306463cdb2ad00cd75208a4
datatypevoid <david.r.niciforovic@gmail.com>
2016-09-27 22:36:45 -0700

    chore: fix e2e test configuration

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -3,7 +3,8 @@
 module.exports = function(config) {
 
   var testWebpackConfig = require('./config/webpack.test.config.js');
-  config.set({
+
+  var configuration = {
 
     // base path that will be used to resolve all patterns (e.g. files, exclude)
     basePath: '',
@@ -63,9 +64,21 @@ module.exports = function(config) {
       'PhantomJS'
     ],
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true
-  });
+  };
 
+  if(process.env.TRAVIS){
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+
+  config.set(configuration);
 };
