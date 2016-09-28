2b42823d96773a131a68a1a02db4a68f34ea2d3f
datatypevoid <david.r.niciforovic@gmail.com>
2016-09-27 21:29:20 -0700

    chore: provide var lib for dev

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -67,7 +67,9 @@ module.exports = webpackMerge(commonConfig, {
     // inside the output.path directory.
     //
     // See: http://webpack.github.io/docs/configuration.html#output-chunkfilename
-    chunkFilename: '[id].chunk.js'
+    chunkFilename: '[id].chunk.js',
+    library: 'ac_[name]',
+    libraryTarget: 'var'
   },
 
   // Add additional plugins to the compiler.
